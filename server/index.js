b74bb7ebbd93a14b58ff460ce5d63dcc63c7d933
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-08-13 18:07:35 -0600

    added client

--- server/index.js
+++ server/index.js
@@ -12,10 +12,25 @@ app.use(express.json());
 
 
 //get all Todos
-
+app.get ("/todos", async (req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    }catch(err) {
+        console.error(err.message);
+    }
+});
 
 //get a todo
-
+app.get("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+        res.json(todo.rows[0]);
+    }catch(err) {
+        console.error(err.message);
+    }
+});
 //create a todo
 
 app.post("/todos", async (req, res) => {
@@ -31,6 +46,18 @@ app.post("/todos", async (req, res) => {
 
 
 //update a todo
+app.put("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+        res.json("Todo was updated");
+    }catch(err) {
+        console.error(err.message);
+    }
+});
+  
+
 
 //delete a todo 
 
@@ -39,3 +66,4 @@ app.listen(5000, () => {
     console.log("Server is starting on port 5000");
 });
 
+  
