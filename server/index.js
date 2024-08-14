d1a3d8acbb81ec99f6713fd8c9022bc29d2ad0b1
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-08-13 18:22:14 -0600

    first pages

--- server/index.js
+++ server/index.js
@@ -60,6 +60,15 @@ app.put("/todos/:id", async (req, res) => {
 
 
 //delete a todo 
+app.delete("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+        res.json("Todo was deleted");
+    }catch(err) {
+        console.error(err.message);
+    }
+});
 
 
 app.listen(5000, () => {
